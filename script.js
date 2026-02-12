77507798eb580868dabd4cef3f793c69246788ab
Bradley Johnson <brdlysjhnsn@gmail.com>
2026-02-12 17:34:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openForm(type) {
+    if (type === "bakery") {
+        alert("Bakery order form coming soon!");
+    } else if (type === "it") {
+        alert("IT service request form coming soon!");
+    }
+}
